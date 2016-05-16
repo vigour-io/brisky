6b7cb65e9505580be645f9d50d5efec298d49528
Youri Daamen <me@youz.io>
2016-05-16 15:30:03 +0200

    inject all events on brisky

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 module.exports = require('brisky-core')
 
 module.exports.prototype.inject(
-  require('brisky-events'),
+  require('brisky-events/rightclick'),
+  require('brisky-events/basic'),
+  require('brisky-events/force'),
+  require('brisky-events/hover'),
+  require('brisky-events/drag'),
+  require('brisky-events/key'),
   require('brisky-props'),
   require('brisky-class'),
   require('brisky-focus'),
