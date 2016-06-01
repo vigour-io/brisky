6cb2ee04d1e6787d1d63ce8bf48ebce4371c4bb1
Youri Daamen <me@youz.io>
2016-06-01 22:52:06 +0200

    render

--- render.js
+++ render.js
@@ -1,3 +1,25 @@
 'use strict'
 require('./')
-module.exports = require('brisky-core/render')
+
+const brender = require('brisky-core/render')
+const ua = require('vigour-ua/navigator')
+const parser = require('case-parser')
+const uacases = require('ua-cases')
+const cases = uacases(ua)
+
+module.exports = function render (elem, state, cb) {
+  elem = parser(elem, cases)
+  addClasses(elem)
+  return brender(elem, state, cb)
+}
+
+function addClasses (elem) {
+  const elemClass = elem.class
+  const classes = elemClass ? typeof elemClass === 'string' ? { val: elemClass } : elemClass : {}
+  for (let i in cases) {
+    if (cases[i]) {
+      classes[i.slice(1)] = true
+    }
+  }
+  elem.class = classes
+}
