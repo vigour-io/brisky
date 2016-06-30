34313d62d0ae02eebd7cb8bed39b685871cd1dae
Jim de Beer <jim@vigour.io>
2016-06-30 11:47:40 +0200

    added tests

--- test/index.js
+++ test/index.js
@@ -1,1 +1,10 @@
 'use strict'
+const render = require('../render')
+const test = require('tape')
+const parse = require('parse-element')
+
+test('render - cases', function (t) {
+  var elem = render({}, false, { $bla: true })
+  t.same(parse(elem), '<div class="bla"></div>', 'add cases')
+  t.end()
+})
