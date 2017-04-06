2ba79ffeef6173b638b06d0055f3e69e428a0d76
Jim de Beer <jim@vigour.io>
2017-04-06 11:36:18 +0200

    add correct dev deps

--- test/index.js
+++ test/index.js
@@ -1,10 +1,7 @@
 'use strict'
 const render = require('../render')
-const test = require('tape')
-const parse = require('parse-element')
 
 test('render - cases', function (t) {
-  var elem = render({}, false, { $bla: true })
   t.same(parse(elem), '<div class="bla"></div>', 'add cases')
   t.end()
 })
