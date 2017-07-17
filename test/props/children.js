20ecb9ea8f4efd44ed630d85402c58b7637f6d78
Jim de Beer <jim@vigour.io>
2017-07-17 14:33:21 +0200

    fixes

--- test/props/children.js
+++ test/props/children.js
@@ -1,10 +1,8 @@
 // const Thing = props => <div>{props.text}</div>
 
 // this is step 1
-const App = props => <div>hello</div>
+const App = props => <div>hello<div>bye</div></div>
 
 // step 2 is switch
-
 // step 3 is getting those expression to work faster / better / stronger
-
 module.exports = App
--- test/props/result/children.js
+++ test/props/result/children.js
@@ -8,6 +8,9 @@ module.exports =
       if (!tree._) tree._ = {}
       var _1 = tree._[1] = document.createElement('div')
       _1.appendChild(document.createTextNode('hello'))
+      var _2 = tree._[2] = document.createElement('div')
+      _1.appendChild(_2)
+      _2.appendChild(document.createTextNode('bye'))
     }
   }
 }
