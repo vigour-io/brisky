9f2ab424153ad7983190c0f0eeda793176afe10b
Mustafa Dokumaci <mustafa@foraa.com>
2017-08-22 01:12:15 +0200

    bring back set get tests

--- test/index.js
+++ test/index.js
@@ -26,44 +26,44 @@ console.log('serialize pointers:', master.get(['pointers']).serialize())
 console.log('serialize deep:', master.get(['deep']).serialize())
 console.log('serialize master:', master.serialize())
 
-// const state = new Struct()
-//
-// const arr = {}
-//
-// const blur = {
-//   val: 'blurblurblur'
-// }
-// var i = 10
-// while (i--) {
-//   blur['x' + i] = 'blur' + i
-// }
-//
-// i = 80
-// while (i--) {
-//   arr[i] = {
-//     val: 'hello' + i,
-//     a: 'a' + i,
-//     b: 'b' + i,
-//     c: 'c' + i,
-//     nested: blur
-//   }
-// }
-//
-// // set property dont check keys totally fine
-//
-// var d = Date.now()
-// state.set(arr)
-// console.log('set + create 100k * 4', Date.now() - d, 'ms')
-//
-// d = Date.now()
-// i = 1
-// var r
-// while (i--) {
-//   r = state.get(2023)
-// }
-// console.log('get 1mil', Date.now() - d, 'ms', r)
-//
-// console.log(state)
-// global.state = state
-//
-// console.log(Object.keys(state.leaves).length)
+const state = new Struct()
+
+const arr = {}
+
+const blur = {
+  val: 'blurblurblur'
+}
+var i = 5
+while (i--) {
+  blur['x' + i] = 'blur' + i
+}
+
+i = 1e4
+while (i--) {
+  arr[i] = {
+    val: 'hello' + i,
+    a: 'a' + i,
+    b: 'b' + i,
+    c: 'c' + i,
+    nested: blur
+  }
+}
+
+// set property dont check keys totally fine
+
+var d = Date.now()
+state.set(arr)
+console.log('set + create 10k * 10', Date.now() - d, 'ms')
+
+d = Date.now()
+i = 1e4
+var r
+while (i--) {
+  r = state.get(i)
+}
+console.log('get 10k', Date.now() - d, 'ms', r)
+
+console.log(state)
+global.state = state
+
+console.log(Object.keys(state.leaves).length)
